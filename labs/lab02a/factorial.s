9364724d31d5055d3711f3b7730bc964785f2928
ccz <321510177@qq.com>
2022-08-30 13:55:55 +0800

    finisha part of lab02

--- labs/lab02a/factorial.s
+++ labs/lab02a/factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 7
 
 .text
 main:
@@ -22,3 +22,13 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    addi t0, x0, 1      # 0
+    add t1, x0, a0      # curr_n = n, ret = n
+loop:
+    beq t1, t0, finish  # exit when curr_n = 1
+    addi t1, t1, -1     # curr_n -= 1
+    mul a0, a0, t1      # ret *= curr_n
+    jal x0, loop
+
+finish:
+    jr ra
